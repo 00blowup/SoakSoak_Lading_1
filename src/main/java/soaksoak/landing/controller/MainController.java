9da52fbe10f08358e10c960f199ed0b616428efc
Haeun Noh <00blowup@gmail.com>
2023-02-03 02:09:26 +0900

    1차 완성

--- src/main/java/soaksoak/landing/controller/MainController.java
+++ src/main/java/soaksoak/landing/controller/MainController.java
@@ -1,4 +1,18 @@
 package soaksoak.landing.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MainController {
+
+    @GetMapping("/")
+    public String main(){
+        return "main";
+    }
+
+    @GetMapping("/kakao")
+    public String kakao(){
+        return "kakao";
+    }
 }
